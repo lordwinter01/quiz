6ea118892222ea70691155410956fe21bc8e206d
lordwinter01 <lordwinter01@gmail.com>
2024-07-18 08:28:46 +0545

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,63 @@
+
+let questions = [
+  {
+  numb: 1,
+  question: "How do you typically react when faced with a stressful situation??",
+  answer: "I seek support from friends or family to help me cope",
+  options: [
+    "I try to stay calm and think logically about the situation",
+"I tend to feel overwhelmed and may become anxious or upset",
+"I look for distractions or engage in activities to take my mind off the stress",
+"I seek support from friends or family to help me cope"
+    
+  ]
+},
+  {
+  numb: 2,
+  question: "How do you handle conflicts with others?",
+  answer: "I confront the person directly and express my feelings.",
+  options: [
+    "I try to understand the other person’s perspective and find a compromise.",
+    "I avoid the conflict and hope it resolves itself.",
+    "I confront the person directly and express my feelings.",
+    "I feel very upset and find it hard to deal with the conflict."
+  ]
+},
+  {
+  numb: 3,
+  question: "What is your approach to making important decisions??",
+  answer: "I make a pros and cons list and think through my options carefully.",
+  options: [
+    "I make a pros and cons list and think through my options carefully.",
+    "I tend to second-guess myself and worry about making the wrong choice.",
+    "I go with my gut feeling or intuition.",
+    "I ask for advice from people I trust before deciding."
+  ]
+},
+  {
+  numb: 4,
+  question: "How do you feel about trying new things or stepping out of your comfort zone?",
+  answer:"I am cautious but willing to try new things occasionally.",
+  options: [
+     "I feel excited and look forward to new experiences.",,
+     "I feel nervous and often prefer to stick to what I know.",
+     " I am cautious but willing to try new things occasionally.",
+     "I feel very anxious and avoid new experiences whenever possible."
+
+
+  ]
+},
+  {
+  numb: 5,
+  question: "How do you manage your emotions when you're feeling down?",
+  answer:"I distract myself with hobbies or activities to lift my mood.",
+  options: [
+   " I try to understand why I’m feeling this way and work through my emotions.",
+  "I often feel overwhelmed and find it hard to cope.",
+  "I distract myself with hobbies or activities to lift my mood.",
+  "I talk to someone about my feelings to help me process them."
+  ]
+
+},
+
+];
